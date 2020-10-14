8d2b6cc82cf084fd6cc91cb790fd28d4e7a0a194
Dragos Varovici <dvarovici.work@gmail.com>
2020-10-14 09:32:45 -0600

    Api controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # get  '/rooms/:id' => 'rooms#show'
   root 'rooms#index'
 
+  get '/api/rooms' => 'api#get_rooms'
+  post '/api/rooms' => 'api#create_rooms'
+  get '/api/room/:id/messages' => 'api#get_messages'
+  post '/api/room/:id/messages' => 'api#create_message'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
